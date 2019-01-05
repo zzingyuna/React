0bd8731a4f5a5932e1f995f32542e4317e9cc4e0
zzingyuna <kya7637@hanmail.net>
2019-01-05 10:16:46 +0900

    ,

---
+++ build/precache-manifest.a5649977f59c0af1a016cc2aba574e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de8af2439498cf5d1b17",
+    "url": "/static/js/main.de8af243.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "de8af2439498cf5d1b17",
+    "url": "/static/css/main.6553fd28.chunk.css"
+  },
+  {
+    "revision": "8febac96bed58a23206e2f99b227215b",
+    "url": "/index.html"
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+  
+  const element = React.createElement(
+    'h1',
+    {className: 'greeting'},
+    'Hello, world!'
+  );
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
+
+  
+  ReactDOM.render(
+    element,
+    document.getElementById('root3')
+  );
+  
+  class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
+    }
+  
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Toggle />,
+    document.getElementById('root3')
+  );
+
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Hello, world!</h1>,
+    document.getElementById('root2'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
