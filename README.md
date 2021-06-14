51bcdabcc9835c6722a6ff1d7c562667f2a9d117
zzingyuna <kya7637@hanmail.net>
2021-06-14 23:04:37 +0900

    .

--- README.md
+++ README.md
@@ -42,3 +42,6 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## babel 추가
+npm install --save-dev @babel/core
--- src/index.js
+++ src/index.js
@@ -5,65 +5,81 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
-  
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-  };
-  
-  const element = React.createElement(
-    'h1',
-    {className: 'greeting'},
-    'Hello, world!'
-  );
+  return user.firstName + ' ' + user.lastName;
+}
 
-  function getGreeting(user) {
-    if (user) {
-      return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = React.createElement(
+  'p',
+  {className: 'greeting'},
+  'Hello, world!'
+);
+
+function getGreeting(user, isToggleOn) {
+  if (user && isToggleOn) {
+    return <p>Hello, {formatName(user)}!</p>;
+  }
+  return <p>Hello, Stranger.</p>;
+}
+
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  
-  ReactDOM.render(
-    element,
-    document.getElementById('root3')
-  );
-  
-  class Toggle extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {isToggleOn: true};
-  
-      // This binding is necessary to make `this` work in the callback
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      this.setState(state => ({
-        isToggleOn: !state.isToggleOn
-      }));
-    }
-  
-    render() {
-      return (
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+    ReactDOM.render(getGreeting(user, this.state.isToggleOn), document.getElementById('userinfo'));
+  }
+
+  render() {
+    return (
+      <div>
+        <span>Who are you?</span>
         <button onClick={this.handleClick}>
           {this.state.isToggleOn ? 'ON' : 'OFF'}
         </button>
-      );
-    }
+        {this.tag}
+      </div>
+    );
   }
-  
-  ReactDOM.render(
-    <Toggle />,
-    document.getElementById('root3')
-  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<h1>Hello, world!</h1>,
-    document.getElementById('root2'));
+ReactDOM.render(<h1>Hello, world! 일반 렌더..</h1>, document.getElementById('hello'));
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('toggle')
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('userinfo')
+);
+
+function tick() {
+  const element = (
+    <div>
+      <p>렌더링 된 요소 업데이트..</p>
+      <p>It is {new Date().toLocaleTimeString()}.</p>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('time'));
+}
+
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
